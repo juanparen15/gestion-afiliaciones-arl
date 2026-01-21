bae5107f3058bf8673869bbe600a8c2e0a63833e
juanparen15 <jprendon9@gmail.com>
2026-01-21 17:22:35 -0500

    nueva funcion tour y vista UX de afiliaciones

--- app/Filament/Resources/AfiliacionResource/Pages/ViewAfiliacion.php
+++ app/Filament/Resources/AfiliacionResource/Pages/ViewAfiliacion.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\AfiliacionResource\Pages;
 
 use App\Filament\Resources\AfiliacionResource;
+use App\Models\Afiliacion;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Support\Facades\Auth;
 
 class ViewAfiliacion extends ViewRecord
 {
@@ -13,7 +15,8 @@ class ViewAfiliacion extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\EditAction::make(),
+            Actions\EditAction::make()
+                ->hidden(fn(Afiliacion $record) => $record->estado !== 'validado' || Auth::user()->hasRole(['super_admin', 'SSST'])),
         ];
     }
 }
