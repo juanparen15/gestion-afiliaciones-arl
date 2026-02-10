06c915b012d450bf213e485a78cde16d30e79752
juanparen15 <jprendon9@gmail.com>
2026-02-10 16:30:31 -0500

    actualizacion de notificacion de proximos a vencer

--- app/Mail/ContratosProximosVencerMail.php
+++ app/Mail/ContratosProximosVencerMail.php
@@ -2,16 +2,14 @@
 
 namespace App\Mail;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ContratosProximosVencerMail extends Mailable implements ShouldQueue
+class ContratosProximosVencerMail extends Mailable
 {
-    use Queueable, SerializesModels;
+    use SerializesModels;
 
     public array $datos;
 
